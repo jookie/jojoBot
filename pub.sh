85c2bd11c38b2fc725db568acf9bc35f075f4420
jookie <dovpeles@gmail.com>
2024-08-13 13:20:34 -0400

    fix commit

---
+++ pub.sh
@@ -0,0 +1,38 @@
+# git init
+# export PATH=/usr/local/share/npm/bin:$PATH
+# git checkout -b my-new-branch
+# …or push an existing repository from the command line
+git remote add origin https://github.com/jookie/jojoBot.git
+git branch -M main
+git push -u origin main
+
+
+# git remote add origin
+# git remote add origin https://github.com/jookie/jojoStock.git
+# git push --set-upstream origin main
+# git branch -M main
+# git push -u origin main
+
+# ssh-keygen -t ed25519 -C "dovpeles@gmail.com"
+
+
+# git remote add origin
+git add .
+git commit -m "fix commit"
+git push -u origin main
+# Creating a New Git Repository on a Hosting Service (e.g., GitHub, GitLab):
+# 1. Create a Repository on the Hosting Service:
+# For GitHub:
+
+# Go to GitHub and log in.
+# 1. Click on the "New" button to create a new repository.
+# 2. Name your repository and add a description.
+   # Do not initialize the repository with a README, .gitignore, or license if you've already initialized your project locally.
+# Connect the Local Repository to the Remote Repository:
+# If you've initialized a local repository and want to connect it to a remote repository on GitHub, GitLab, etc., follow these steps:
+## git remote add origin <remote_repository_URL>
+## 👇️ install yarn cli globally
+# npm install -g yarn
+
+
+
