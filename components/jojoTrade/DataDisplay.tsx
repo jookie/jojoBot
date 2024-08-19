770550a7490581c2eb43198f66c03b79c06cdcda
jookie <dovpeles@gmail.com>
2024-08-19 17:10:15 -0400

    fix commit

--- components/jojoTrade/DataDisplay.tsx
+++ components/jojoTrade/DataDisplay.tsx
@@ -1,6 +1,7 @@
 // components/DataDisplay.tsx
 import React, { useEffect, useState } from 'react'
 
+
 const DataDisplay: React.FC = () => {
   const [data, setData] = useState<string | null>(null)
 
