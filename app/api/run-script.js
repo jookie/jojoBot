d5185efd0a4e63ed7c130138e15858bebc3af631
jookie <dovpeles@gmail.com>
2024-08-19 22:12:26 -0400

    fix commit

--- app/api/run-script.js
+++ app/api/run-script.js
@@ -2,7 +2,7 @@
 import { exec } from 'child_process';
 
 export default function handler(req, res) {
-  exec('python3 path_to_your_script.py', (error, stdout, stderr) => {
+  exec('python3 ./../../scripts/train.py', (error, stdout, stderr) => {
     if (error) {
       res.status(500).json({ error: stderr });
     } else {
