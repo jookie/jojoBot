5374ccb93cf8ce83bf1957c56c6f6c3f08f68869
jookie <dovpeles@gmail.com>
2024-09-25 18:31:19 -0400

    vercel-fix

--- pub.sh
+++ pub.sh
@@ -14,8 +14,6 @@ git push -u origin main
    # Do not initialize the repository with a README, .gitignore, or license if you've already initialized your project locally.
 # --------Remove Repository:
 # rm -rf .git
-# git init
-# If you've initialized a local repository and want to connect it to a remote repository on GitHub, GitLab, etc., follow these steps:
 ## git remote add origin <remote_repository_URL>
 ## 👇️ install yarn cli globally
 # npm install -g yarn
